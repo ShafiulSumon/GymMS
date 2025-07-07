789b9afa10a6cb557787269ee747250c3b44729b
Shafiul Alam <shafiul.alam@bjitgroup.com>
2025-07-07 17:06:05 +0600

    Working on MemberEntity

---
+++ GymMS/GymMS.Core/Entities/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymMS.Core.Entities
+{
+    public class BaseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public bool IsDeleted { get; set; } = false;
+    }
+}
